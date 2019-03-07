3a3c48bdfe574650f2d1f3913b073ae6be6d7cd7
kofi <mokomekofi@yahoo.com>
2019-03-07 23:30:44 +0100

    Add Quil Editor

--- src/api/save_slides.php
+++ src/api/save_slides.php
@@ -7,15 +7,25 @@ $request = json_decode($postdata);
 $user_id = $request->user_id;
 $presentation_id = $request->presentation_id;
 
-$query = "select owner_id from presentations where id = {$presentation_id} and deleted = 0";
+
+foreach ($request->slides as $slide) {
+    $query = "update slides set content = '{$slide->content}' where id={$slide->id}";
+    if (!$result = $con->query($query)) {
+        echo json_encode(array("message" => $con->error, "success" => false));
+        die();
+    }
+    //echo $slide->id." : ".$slide->content." \n";
+}
+
+echo json_encode(array("message" => "OK", "success" => true));
+/*$query = "select owner_id from presentations where id = {$presentation_id} and deleted = 0";
 if ($result = $con->query($query)) {
     $result = $result->fetch_object();
     if ($result->owner_id == $user_id) {
         $query = "select id,content from slides where presentation_id = {$presentation_id} and deleted = 0";
         if ($result = $con->query($query)) {
             $slides = [];
-            while ($row = $result->fetch_object()) {
-                //$row->content = json_encode($row->content);
+            while ($row = $result->fetch_assoc()) {
                 array_push($slides, $row);
             }
             $data = array(
@@ -28,9 +38,9 @@ if ($result = $con->query($query)) {
             echo json_encode(array("success" => false, "message" => "an error occurred please try again", "code" => 0));
         }
     } else {
-        echo json_encode(array("message" => "You are not allowed to edit this slide", "success" => false, "code" => 404));
+       echo json_encode(array("message" => "You are not allowed to edit this slide", "success" => false, "code" => 404));
     }
 } else {
     echo json_encode(array("message" => $con->error, "success" => false, "code" => 0));
-}
+}*/
 
--- src/app/presentation/edit/edit.component.ts
+++ src/app/presentation/edit/edit.component.ts
@@ -3,7 +3,8 @@ import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {UserService} from "../../services/user.service";
-import {log} from "util";
+import * as Quill from "../../../../node_modules/quill";
+import * as Delta from "../../../../node_modules/quill-delta";
 
 @Component({
     selector: 'app-edit',
@@ -15,11 +16,17 @@ export class EditComponent implements OnInit {
     private loading;
     private slides: any[];
     private slide_content: string;
+    private editor: any;
+    private editor_initialised;
+    private current_slide: any;
+    private current_index;
 
     constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {
     }
 
     ngOnInit() {
+        this.current_slide = null;
+        this.editor_initialised = false;
         this.loading = true;
         this.slide_content = '';
         this.route.params.subscribe((params: ParamMap) => {
@@ -35,9 +42,12 @@ export class EditComponent implements OnInit {
         })
             .subscribe((response: any) => {
                     if (response.success) {
-                        //console.log(response);
+                        console.log(response);
+                        for (let i = 0; i < response.slides.length; i++) {
+                            //response.slides[i].content = new Delta(JSON.parse(response.slides[i].content));
+                        }
                         this.slides = response.slides;
-                        console.log(this.slides);
+                        //console.log(this.slides);
                         this.loading = false;
 
                     } else {
@@ -79,8 +89,61 @@ export class EditComponent implements OnInit {
             );
         this.slides.push(1);
     }
-    setContent(content: string){
-        this.slide_content = content;
+
+    setContent(slide: any, index: any) {
+        if (this.current_slide == null) {
+            this.current_slide = slide;
+        } else {
+            this.slides[this.current_index].content = this.editor.getContents();
+        }
+        if (!this.editor_initialised) {
+            this.editor = new Quill('#editor', {
+                theme: 'snow'
+            });
+
+            this.editor_initialised = true;
+        }
+        this.current_index = index;
+        this.editor.setContents(slide.content);
+
+    }
+
+    saveSlides() {
+        if (this.current_slide != null) {
+            //this.slides[this.current_index].content = this.editor.getContents();
+        }
+
+        let temp_slides = JSON.parse(JSON.stringify(this.slides));
+        for (let i = 0; i < temp_slides.length; i++) {
+            temp_slides[i].content = JSON.stringify(temp_slides[i].content);
+        }
+        let data = {
+            user_id: UserService.getUserId(),
+            presentation_id: this.edit_id,
+            slides: temp_slides,
+        };
+
+        //console.log(data.slides);
+        this.http.post(environment.apiRoot + 'save_slides.php', data, {
+            headers: new HttpHeaders().set('Access-Control-Allow-Headers', '*')
+        })
+            .subscribe((response: any) => {
+                    if (response.success) {
+                        console.log(response);
+
+                    } else {
+                        console.log(response);
+                    }
+
+                },
+                (error) => {
+                    console.error('Failed decline request ', error);
+                },
+            );
+    }
+
+    startPresentation() {
+
     }
 
 }
