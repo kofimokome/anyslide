7eef5c47132065e8c36ec4647dae1e44dcc91709
kofi <mokomekofi@yahoo.com>
2019-03-09 23:05:28 +0100

    Add Online Api Server Link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiRoot: 'https://cors-anywhere.herokuapp.com/http://anyslide.kofimokome.ml',
+    apiRoot: 'https://cors-anywhere.herokuapp.com/http://anyslide.kofimokome.ml/',
     serverUrl: 'https://anyslide.herokuapp.com/'
 };
